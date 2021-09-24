4f2d547f478c9f03b731cabb3dabc5e8b6ef193f
vson <vson@itu.dk>
2021-09-24 13:00:08 +0200

    Add exercise 1

--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections.Generic;
 
 namespace BDSA2020.Assignment02
 {
     public static class Extensions
     {
+
+        //public static IEnumerable<T> Flatten(this IEnumerable<T>[] input)
     }
 }
