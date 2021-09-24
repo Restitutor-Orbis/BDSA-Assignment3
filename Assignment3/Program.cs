143f40218170861fe7a8f6e6ef08a8e7d79eef2d
Restitutor-Orbis <65425000+Restitutor-Orbis@users.noreply.github.com>
2021-09-24 14:03:52 +0200

    Extensions (#1)
    * Add exercise 1
    
    * Finish shit

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using static System.Console;
 
 namespace Assignment3
 {
@@ -6,7 +10,15 @@ namespace Assignment3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] ys = new int[] {1,2,3,4,5,6,7, 49, 56, 60};
+            //exercise 1.1
+            //var xs = ys.SelectMany(i => i);
+
+            //exercise 1.2
+            //var xs = ys.Where(i => i % 7 == 0 && i > 42);
+
+            //exercise 1.3
+            //var xs = ys.Where(i => i % 4 == 0);
         }
     }
 }
