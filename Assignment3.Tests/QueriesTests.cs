633417bac929c63a5bb62414d7cb01a74605be19
xZenshin <askoularsen@gmail.com>
2021-09-27 13:04:15 +0200

    add implementation for first 2 linq exercises

--- Assignment3.Tests/QueriesTests.cs
+++ Assignment3.Tests/QueriesTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace BDSA2020.Assignment03.Tests
@@ -6,9 +7,47 @@ namespace BDSA2020.Assignment03.Tests
     public class QueriesTests
     {
         [Fact]
-        public void Test1()
+        public void only_return_names_of_wizards_invented_by_rowling_linq()
         {
+            var queries = new Queries();
+            var wizards = Wizard.Wizards.Value;
+            var expected = new List<string> {"Troldmand1", "Troldmand2"};
 
+            var actual = queries.returnNamesInventedByRowlingLinq(wizards);
+
+            Assert.Equal(expected, actual);
+        }   
+
+        [Fact]
+        public void only_return_names_of_wizards_invented_by_rowling_extension() {
+            var queries = new Queries();
+            var wizards = Wizard.Wizards.Value;
+            var expected = new List<string> {"Troldmand1", "Troldmand2"};
+
+            var actual = queries.returnNamesInventedByRowlingExtension(wizards);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void return_the_first_sithlord_linq() {
+            var queries = new Queries();
+            var wizards = Wizard.Wizards.Value;
+            var expected = 1977;
+
+            var actual = queries.FindFirstSithLordLinq(wizards);
+
+            Assert.Equal(expected,actual);
+        }
+                [Fact]
+        public void return_the_first_sithlord_extension() {
+            var queries = new Queries();
+            var wizards = Wizard.Wizards.Value;
+            var expected = 1977;
+
+            var actual = queries.FindFirstSithLordExtension(wizards);
+
+            Assert.Equal(expected,actual);
         }
     }
 }
--- Assignment3/Queries.cs
+++ Assignment3/Queries.cs
@@ -1,6 +1,39 @@
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
 namespace BDSA2020.Assignment03
 {
     public class Queries
     {
+
+        public IEnumerable<string> returnNamesInventedByRowlingLinq(IEnumerable<Wizard> wizards) {
+
+            var output = from w in wizards
+                         where w.Creator.Equals("J.K Rowling")
+                         select w.Name;
+
+            return output; 
+        }
+
+        public IEnumerable<string> returnNamesInventedByRowlingExtension(IEnumerable<Wizard> wizards) {
+
+            var output = wizards.Where(w => w.Creator.Equals("J.K Rowling")).Select(w => w.Name);
+            return output;
+        }
+          
+        public int? FindFirstSithLordLinq(IEnumerable<Wizard> wizards) 
+        {
+            var output = (from w in wizards
+                         where w.Name.Contains("Darth")
+                         orderby w.Year
+                         select w.Year).First();
+            return output;
+        }  
+
+        public int? FindFirstSithLordExtension(IEnumerable<Wizard> wizards) {
+            var output = wizards.Where(w => w.Name.Contains("Darth")).Select(w => w.Year).First();
+            return output;
+        }
     }
 }
